b26cc64adc03880c87a8b27ffc34e6b2f6898c24
Juanda1996 <judperezre@unal.edu.co>
2023-01-19 23:47:53 -0500

    Initial commit initializing all the domains

---
+++ CRUD/bankApp.domain/Interfaces/Repositories/IRepositoyClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankApp.domain.Interfaces.Repositories
+{
+	internal interface IRepositoyClient
+	{
+	}
+}
